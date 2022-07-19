faba096ed0730963bd4b4c9a4bab23ff6287ba45
LeeJuYeol <37298646+lamerGit@users.noreply.github.com>
2022-07-19 13:43:56 +0900

    늑대인간 수정
    한번클릭하면 1,2번 공격 같이나가는 버그 수정 및 합치기

--- Assets/Scripts/PlayerWolf.cs
+++ Assets/Scripts/PlayerWolf.cs
@@ -11,10 +11,18 @@ public class PlayerWolf : MonoBehaviour , IHealth
     Vector3 inputDir = Vector3.zero;
     public float turnSpeed = 10.0f;
 
+    public float forwardJumpPower = 3.0f;
+    public float upJumpPower = 10.0f;
+    public int jumpTime = 2;
+    public float skillContinueTime = 10.0f;
+    int tempJumpTime;
+
+
     public float moveSpeed = 3.0f;
     Quaternion targetRotation = Quaternion.identity;
 
     Animator anim = null;
+    ParticleSystem SkillAura;
 
     float Player_Hp = 100.0f;
     float Player_MaxHp = 100.0f;
@@ -24,9 +32,16 @@ public class PlayerWolf : MonoBehaviour , IHealth
 
     private void Awake()
     {
+        SkillAura = GetComponentInChildren<ParticleSystem>();
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
  
+    }
+
+    private void Start()
+    {
+        tempJumpTime = jumpTime;
+        //SkillAura.Stop();
     }
 
     private void FixedUpdate()
@@ -111,7 +126,7 @@ public class PlayerWolf : MonoBehaviour , IHealth
 
     public void OnAttackInput(InputAction.CallbackContext context)
     {
-        if (!GameManager.INSTANCE.CAMERASWAP)
+        /*if (!GameManager.INSTANCE.CAMERASWAP)
         {
             if (context.performed)
             {
@@ -121,10 +136,57 @@ public class PlayerWolf : MonoBehaviour , IHealth
             {
                 anim.SetBool("isAttack", false);
             }
+        }*/
+        if (!GameManager.INSTANCE.CAMERASWAP)
+        {
+            anim.SetFloat("ComboState", Mathf.Repeat(anim.GetCurrentAnimatorStateInfo(0).normalizedTime, 1.0f));
+            anim.ResetTrigger("isAttack");
+            anim.SetTrigger("isAttack");
+            anim.SetBool("isAttackM", true);
         }
     }
 
-   
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (!GameManager.INSTANCE.CAMERASWAP)
+        {
+            if (jumpTime > 0 && context.started)
+            {
+                anim.ResetTrigger("isJump");
+                anim.SetTrigger("isJump");
+
+                rigid.AddForce(transform.up * upJumpPower + transform.forward * forwardJumpPower, ForceMode.Impulse);
+                jumpTime--;
+            }
+        }
+
+    }
+
+    public void OnSkillInput(InputAction.CallbackContext context)
+    {
+        if (!GameManager.INSTANCE.CAMERASWAP)
+        {
+            StartCoroutine(SkillAuraOnOff());
+            anim.SetBool("isSkill", true);
+        }
+
+    }
+
+    IEnumerator SkillAuraOnOff()
+    {
+        //gameObject.GetComponentInChildren<ParticleSystem>().
+        SkillAura.Play();
+        yield return new WaitForSeconds(skillContinueTime);
+        SkillAura.Stop();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpTime = tempJumpTime;
+        }
+    }
 
     // HP�ѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤ�
     public float HP
@@ -136,7 +198,11 @@ public class PlayerWolf : MonoBehaviour , IHealth
         set
         {
             Player_Hp = Mathf.Clamp(value, 0, Player_MaxHp);
+            
+            
             onHealthChange?.Invoke();
+
+            
             //Debug.Log(Player_Hp);
         }
 
@@ -160,6 +226,22 @@ public class PlayerWolf : MonoBehaviour , IHealth
             finalDamage = 1.0f;
         }
         HP -= finalDamage;
+        if(HP<0.1f)
+        {
+            Die();
+        }else
+        {
+            Hit();
+        }
+        
     }
 
+    public void Hit()
+    {
+        anim.SetTrigger("hit");
+    }
+    void Die()
+    {
+        anim.SetTrigger("Die");
+    }
 }
