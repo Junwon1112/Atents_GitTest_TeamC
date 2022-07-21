ff850bd2d5e44028441fd70abac79f110562d866
Junwon1112 <105641787+Junwon1112@users.noreply.github.com>
2022-07-21 15:25:21 +0900

    마우스따라 회전 수정

--- Assets/Scripts/PlayerWolf.cs
+++ Assets/Scripts/PlayerWolf.cs
@@ -16,6 +16,7 @@ public class PlayerWolf : MonoBehaviour , IHealth
     public int jumpTime = 2;
     public float skillContinueTime = 10.0f;
     int tempJumpTime;
+    
 
 
     public float moveSpeed = 3.0f;
@@ -73,16 +74,25 @@ public class PlayerWolf : MonoBehaviour , IHealth
                 anim.SetBool("isMove", false);
             }
             Vector3 mousePos = Mouse.current.position.ReadValue();
+            //Debug.Log($"{mousePos}"); //마우스 좌표 : x,y값 받아옴, z는 0 : 고정된 값
             Ray cameraRay = PlayerCamera.ScreenPointToRay(mousePos);
-            Plane GroupPlane = new Plane(Vector3.up, Vector3.zero);
+            Plane GroupPlane = new Plane(transform.forward, -10000);
+            
             float rayLength;
             if(GroupPlane.Raycast(cameraRay,out rayLength))
             {
                 Vector3 pointTolook = cameraRay.GetPoint(rayLength);
+                //Debug.Log($"{pointTolook}"); // 레이를 이용해 xz값으로 바꿈, y는 0 : 마우스를 멈춰도 변화하는 값
+
                 Vector3 LookDir=(pointTolook- transform.position).normalized;
-                LookDir.y = 0.0f;
-                LookDir.x = Mathf.Clamp(LookDir.x, -80.0f, 80.0f);
                 
+               
+                //LookDir.x = Mathf.Clamp(LookDir.x, -80.0f, 80.0f);
+                //LookDir.z = LookDir.y;
+                //LookDir.y = 0.0f;
+
+                Debug.Log($"{LookDir}");
+
                 transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(LookDir), Time.fixedDeltaTime*testSpeed);
                 
                 //transform.LookAt(new Vector3(pointTolook.x, transform.position.y, pointTolook.z));
