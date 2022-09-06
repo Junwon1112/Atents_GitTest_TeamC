df66be6b479825364f10dc5150131ee4d6f9b344
Sundog585 <100826717+Sundog585@users.noreply.github.com>
2022-09-06 21:34:59 +0900

    트랩 이팩트 미완성
    트랩 기능은 완성을 했는데 이팩트가 문제가 있습니다. 몬스터가 밟으면 폭발 이팩트가 작동하게 하고 싶은데 시작할때 터지기만 하네요.. 여러가지 시도를 해봤지만 똑같습니다 ㅠㅠ

---
+++ Assets/Scripts/Item/Trap.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public ParticleSystem trapIsHere;
+    public GameObject trapEffect;
+    //float trapDamage = 50.0f;
+    //Monster monster;
+
+    private void Start()
+    {
+        trapIsHere = GetComponentInChildren<ParticleSystem>();
+        //trapEffect.SetActive(false);
+        //monster = GetComponent<Monster>();
+        gameObject.transform.parent = null;
+        trapIsHere.Play();
+        //trapEffect.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            //particles[1].;
+            trapEffect.gameObject.SetActive(true);
+            trapIsHere.Stop();
+            DeleteChilds();
+            Destroy(this);
+            collision.GetComponent<Monster>().Die();
+        }
+    }
+
+    void DeleteChilds()
+    {
+        Transform[] childList = this.GetComponentsInChildren<Transform>();
+        if(childList != null)
+        {
+            for(int i = 1; i<childList.Length; i++)
+            {
+                if(childList[i] != transform)
+                {
+                    Destroy(childList[i].gameObject);
+                }
+            }
+        }
+    }
+}
